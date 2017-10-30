b1bf3b4a6189179666049f825f1d0db733ad8dea
geoxneo <al160000@alumnos.uacj.mx>
2017-10-30 13:14:12 -0600

    Servicio de autenticación

--- src/providers/aut-sesi/aut-sesi.ts
+++ src/providers/aut-sesi/aut-sesi.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-
+import {Observable} from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 /*
   Generated class for the AutSesiProvider provider.
 
@@ -11,8 +13,32 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AutSesiProvider {
 
-  constructor(public http: Http) {
+  constructor(public http: Http,private firebaseAuth:AngularFireAuth) {
     console.log('Hello AutSesiProvider Provider');
   }
-
+  registrarse(correo:string,clave:string){
+    this.firebaseAuth
+    .auth
+    .createUserWithEmailAndPassword(correo,clave)
+    .then(value=>{console.log('Ok, registrado',value)})
+    .catch(
+        err=>{console.log('Existe un error:',err.message);
+        });
+}
+iniciarSesion(correo:string,clave:string){
+    this.firebaseAuth
+    .auth
+    .signInWithEmailAndPassword(correo,clave)
+    .then(value=>{
+        console.log('Bien, has ingresado');
+    })
+    .catch(err=>{
+        console.log('Algo no anda bien, codigo de error:',err.message);
+    });
+} 
+cerrarSesion(){
+    this.firebaseAuth
+    .auth
+    .signOut();
+}
 }
