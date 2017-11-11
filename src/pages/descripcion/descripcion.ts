1f56e0805e61ab3bdfce43f292b193d4f10e0919
geoxneo <al160000@alumnos.uacj.mx>
2017-11-11 14:37:39 -0700

    Revert "correción 1"
    This reverts commit 4c39628694760204890a32360aaf171240c65145.

--- src/pages/descripcion/descripcion.ts
+++ src/pages/descripcion/descripcion.ts
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { HomePage } from './../home/home';
+import * as $ from 'jquery';
+
+=======
 import { MyApp } from './../../app/app.component';
 import { HomePage } from './../home/home';
 import { Component, Injectable} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+>>>>>>> KML
 /**
  * Generated class for the DescripcionPage page.
  *
@@ -20,16 +29,36 @@ export class DescripcionPage {
 descripcion:Object;
 map:Object;
   
+<<<<<<< HEAD
+constructor(public navCtrl: NavController, public navParams: NavParams,
+   public homePage: HomePage) {
+=======
 constructor(public navCtrl: NavController, public navParams: NavParams, public myApp: MyApp, public Map:HomePage) {
+>>>>>>> KML
   this.descripcion=navParams.data;
   this.map=this.map;
   }
 
+<<<<<<< HEAD
+  KML(nKml){
+    
+    this.navCtrl.push(HomePage);
+    
+    $(document).ready(() => {
+        this.map = this.homePage.ionViewDidLoad();
+
+        //window.alert('map = '+this.map);
+        
+        this.homePage.InsertarKML(this.map, nKml);
+    });
+  }
+=======
  MostrarKML(){
   this.myApp.irAPagina(HomePage);
   
   this.Map.MKML();
   
  }
+>>>>>>> KML
 
 }
