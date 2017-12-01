0b5304a463e73c80d1ea4a73bfab4e97091403e1
Slar04 <rigo_lozano@hotmail.com>
2017-11-30 23:12:09 -0600

    master

--- src/pages/inicio-sesion/inicio-sesion.ts
+++ src/pages/inicio-sesion/inicio-sesion.ts
@@ -3,64 +3,66 @@ import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angu
 import { NgForm } from '@angular/forms';
 import { AutSesiProvider } from './../../providers/aut-sesi/aut-sesi';
 
-@IonicPage()
+@IonicPage() 
+
 @Component({
   selector: 'page-inicio-sesion',
   templateUrl: 'inicio-sesion.html',
 })
 export class InicioSesionPage {
   usuario={email:'',password:''};
-  
-    constructor(public navCtrl: NavController,
-       public navParams: NavParams
-       ,public autSesi:AutSesiProvider,
-       public alertCtrl:AlertController
-    ) {
-    }
-  
-    usuarioNuevo(){
-     this.autSesi.registrarse(this.usuario.email,this.usuario.password)
-     .then((usuario)=>{
-       let alert=this.alertCtrl.create({
-        title:'Registro aceptado',
-        message:'Su registro es satisfactorio',
-        buttons:['Aceptar']
-       });
-       alert.present();
-     })
-     .catch((err)=>{
-       let alert=this.alertCtrl.create({
+
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams
+     ,public autSesi:AutSesiProvider,
+     public alertCtrl:AlertController
+  ) {
+  }
+
+  usuarioNuevo(){
+   this.autSesi.registrarse(this.usuario.email,this.usuario.password)
+   .then((usuario)=>{
+     let alert=this.alertCtrl.create({
+      title:'Registro aceptado',
+      message:'Su registro es satisfactorio',
+      buttons:['Aceptar']
+     });
+     alert.present();
+   })
+   .catch((err)=>{
+     let alert=this.alertCtrl.create({
+      title:'Error',
+      subTitle: err.message,
+      buttons:['Aceptar']
+     });
+     alert.present();
+   })
+   
+  }
+  inicioDeSesion(){
+    this.autSesi.iniciarSesion(this.usuario.email,this.usuario.password)
+    .then((email)=>{
+    let alert=this.alertCtrl.create({
+      title:'Bienvenido',
+      message:'Visite nuestra pagina',
+      buttons:['Aceptar']
+     });
+     alert.present();
+    })
+    .catch((err)=>{
+      let alert=this.alertCtrl.create({
         title:'Error',
         subTitle: err.message,
         buttons:['Aceptar']
        });
        alert.present();
-     })
-     
-    }
-    inicioDeSesion(){
-      this.autSesi.iniciarSesion(this.usuario.email,this.usuario.password)
-      .then((email)=>{
-      let alert=this.alertCtrl.create({
-        title:'Bienvenido',
-        message:'Visite nuestra pagina',
-        buttons:['Aceptar']
-       });
-       alert.present();
-      })
-      .catch((err)=>{
-        let alert=this.alertCtrl.create({
-          title:'Error',
-          subTitle: err.message,
-          buttons:['Aceptar']
-         });
-         alert.present();
-      })
-  
-      
-    }
-  
-    ionViewDidLoad() {
-      console.log('ionViewDidLoad InicioSesionPage');
-    }  
+    })
+
+    
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad InicioSesionPage');
+  }
+
 }
